f4d075c2b6787b707062023b9c6f4937fb768350
sahil0209 <sahilharjai2001@gmail.com>
2023-03-20 23:41:03 +0530

    table update

--- models/departments.js
+++ models/departments.js
@@ -2,15 +2,19 @@ const Sequelize = require('sequelize');
 const db = require('../util/db');
 
 const Department = db.define('department', {
-    department_id: {
+    id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
+        // allowNull: false,
+        // primaryKey: true
+    },
+    department_id: {
+        type: Sequelize.STRING,
+        // autoIncrement: true,
         allowNull: false,
         primaryKey: true
     },
-    department_name: Sequelize.STRING,
-    department_sub_name: Sequelize.STRING,
-    department_sub_sub_name: Sequelize.STRING
+    department_name: Sequelize.STRING
 
 });
 
